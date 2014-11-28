5f12186d42b8177e2b8e03bc8f0f195d78090fd4
Mario Nebl <mario.nebl@sinnerschrader.com>
2014-11-28 12:06:15 +0100

    Internationalize demo popover

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,18 +3,10 @@ var onSubmit = require('./on-submit');
 var onChange = require('./on-change');
 
 document.addEventListener('DOMContentLoaded', function(){
-	dothivify({
-		queries: ['.h0'],
-		diameter: '10px',
-		dotBackground: '#730C61',
-		injectStyles: false
-	});
+	var optionsEl = document.getElementsByClassName('js_snippet_defaults')[0];
+	var options = JSON.parse(optionsEl.textContent);
+	dothivify(options);
 
 	document.addEventListener('submit', onSubmit, false);
 	document.addEventListener('change', onChange, false);
 });
-
-var targets = document.getElementsByClassName("stage-title");
-	window.onload = function() {
-		targets[0].className = targets[0].className + ' animated fadeInDown';
-};
--- public/index.js
+++ public/index.js
@@ -4,21 +4,13 @@ var onSubmit = require('./on-submit');
 var onChange = require('./on-change');
 
 document.addEventListener('DOMContentLoaded', function(){
-	dothivify({
-		queries: ['.h0'],
-		diameter: '10px',
-		dotBackground: '#730C61',
-		injectStyles: false
-	});
+	var optionsEl = document.getElementsByClassName('js_snippet_defaults')[0];
+	var options = JSON.parse(optionsEl.textContent);
+	dothivify(options);
 
 	document.addEventListener('submit', onSubmit, false);
 	document.addEventListener('change', onChange, false);
 });
-
-var targets = document.getElementsByClassName("stage-title");
-	window.onload = function() {
-		targets[0].className = targets[0].className + ' animated fadeInDown';
-};
 
 },{"../../src/":6,"./on-change":2,"./on-submit":3}],2:[function(require,module,exports){
 var scrollTo = require('animated-scrollto');
